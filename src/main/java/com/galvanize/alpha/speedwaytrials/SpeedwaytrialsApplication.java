ed75d69a33570d89d2bc888d7f5b48fdec8dcb17
Vinay <vinay.py@gmail.com>
2021-04-19 19:15:57 -0400

    First commit

---
+++ src/main/java/com/galvanize/alpha/speedwaytrials/SpeedwaytrialsApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.alpha.speedwaytrials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedwaytrialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedwaytrialsApplication.class, args);
+	}
+
+}
