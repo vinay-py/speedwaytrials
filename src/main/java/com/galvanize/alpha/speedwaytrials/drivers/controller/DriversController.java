6ec8b65f3b9614dd1e07dc87c2de9a4494dbb936
j-tanz <john.tansor@cognizant.com>
2021-04-21 19:01:52 -0400

    adds successfull test/implementation for get all drivers when empty

--- src/main/java/com/galvanize/alpha/speedwaytrials/drivers/controller/DriversController.java
+++ src/main/java/com/galvanize/alpha/speedwaytrials/drivers/controller/DriversController.java
@@ -4,12 +4,21 @@ import com.galvanize.alpha.speedwaytrials.drivers.models.DriverDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("drivers")
 public class DriversController {
+
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public String addADriver(@RequestBody DriverDto driverDto) {
         return "Driver Added Successfully";
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<DriverDto> getAllDrivers() {
+        return List.of();
     }
 }
